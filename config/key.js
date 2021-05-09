bf20a5a99173f96256fd3ed83deb716874bdeb80
duonggiang152 <62105435+duonggiang152@users.noreply.github.com>
2021-05-09 23:26:37 +0700

    second comit

--- config/key.js
+++ config/key.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: 'localhost',
-    password: '',
-    user: 'root',
-    database: 'todolistver1'
+    host: 'bqyhe9oh6rw51hli1bgz-mysql.services.clever-cloud.com',
+    database: 'bqyhe9oh6rw51hli1bgz',
+    user: 'uc1gccljhgrb1vvs',
+    password: 'UT8O8BvOLIlNL1wmMD1I',
 }
